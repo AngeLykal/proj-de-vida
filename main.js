a562c8a225e66c4e812327f2ecba650074f03115
AngeLykal <116352876+AngeLykal@users.noreply.github.com>
2024-05-17 10:44:30 +0000

    alteraçoes

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 const botoes = document.querySelectorAll("botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function() {
 
-for(let j=0;j<botoes.length;j++) {
-botoes[i].classList.add("ativo");
-botoes[j].classList.remove("ativo");
-}
-
-for(let i=0;i <botoes.length;i++){
+for(let i=0;i <botoes.length;i++) {
     botoes[i].onclick = function() {
-    botoes[i].classList.add(ativo);
-    textos[i].classList.add(ativo);
-    textos[j].classList.remove(ativo);
+for(let j=0;j<botoes.length;j++) {
+    botoes[j].classList.remove("ativo");
+    textos[j].classList.remove("ativo");
+}
+    textos[i].classList.add("ativo");
+    botoes[i].classList.add("ativo");
 }
 }
+
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-0o5,T0o0,0o0,0o0");
 
@@ -30,6 +28,4 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
 return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
-}
-}
 }
