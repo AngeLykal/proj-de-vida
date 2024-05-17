9b072ada3f5a400d06619f87451c5346284a9737
AngeLykal <116352876+AngeLykal@users.noreply.github.com>
2024-05-17 11:01:32 +0000

    alkteraçoes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const botoes = document.querySelectorAll("botao");
+const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 console.log(botoes)
@@ -8,13 +8,13 @@ for(let j=0;j<botoes.length;j++) {
     botoes[j].classList.remove("ativo");
     textos[j].classList.remove("ativo");
 }
-    textos[i].classList.add("ativo");
     botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
 }
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-0o5,T0o0,0o0,0o0");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
